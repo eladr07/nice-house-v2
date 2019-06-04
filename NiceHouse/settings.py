a12e68883c322ac01e26e013245c9cadaea80025
Elad <eladr07@gmail.com>
2019-06-04 09:04:21 +0300

    update host

--- NiceHouse/settings.py
+++ NiceHouse/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
     'nicehouse.herokuapp.com',
-    'nicehouse-env.emfymi2hpr.eu-central-1.elasticbeanstalk.com'
+    'nicehouse-env-2.piih2mkuip.eu-central-1.elasticbeanstalk.com'
     ]
 
 
