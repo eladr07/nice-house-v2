3ff7f64d8799fbdff9f6eb4c2658d4ec331149ac
Elad Reuveni <elad@nummularii.com>
2019-06-25 12:06:54 +0300

    fixed logging

--- NiceHouse/settings.py
+++ NiceHouse/settings.py
@@ -214,22 +214,3 @@ LOGGING = {
         },
     }
 }
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/path/to/django/debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
