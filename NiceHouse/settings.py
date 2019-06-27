6b7be5fac837748cd80f3a29474a7061502fd15d
Elad Reuveni <elad@nummularii.com>
2019-06-27 07:35:38 +0300

    added RevisionMiddleware

--- NiceHouse/settings.py
+++ NiceHouse/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'reversion.middleware.RevisionMiddleware',
 ]
 
 ROOT_URLCONF = 'NiceHouse.urls'
