71fe63ebd99500746bbfb0bca148b0c421b73063
Elad Reuveni <elad@nummularii.com>
2019-07-09 10:14:47 +0300

    log-fix

--- Management/models.py
+++ Management/models.py
@@ -1111,9 +1111,11 @@ class EmployeeSalary(EmployeeSalaryBase):
                     s.employee_paid = True
                     s.save() 
         except:
+            logger.info("ERROR")
             logger.exception('exception while trying to calculate salary for employee %(employee)s, year %(year)s, month %(month)s',
                              {'employee':self.employee, 'year':self.year, 'month':self.month})
         else:
+            logger.info("SUCCESS")
             logger.info('succeeded to calculate salary for employee %(employee)s, year %(year)s, month %(month)s',
                         {'employee':self.employee, 'year':self.year, 'month':self.month})
             
