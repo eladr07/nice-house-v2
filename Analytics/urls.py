61a7891e4223343766308659aa1170cd2924dc11
Elad <eladr07@gmail.com>
2019-07-23 12:19:59 +0300

    changed urls

--- Analytics/urls.py
+++ Analytics/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('saleanalysis/', views.sale_analysis, name='sale-analysis'),
-    path('nhseasonprofit/', views.nh_season_profit, name='nh-season-profit'),
-    path('seasonincome/', views.season_income, name='season-income'),
-    path('profitloss', views.global_profit_lost, name='profit-loss'),
-    path('projectsprofit', views.projects_profit, name='projects-profit'),
+    path('sale-analysis/', views.sale_analysis, name='sale-analysis'),
+    path('nh-season-profit/', views.nh_season_profit, name='nh-season-profit'),
+    path('season-income/', views.season_income, name='season-income'),
+    path('profit-loss', views.global_profit_lost, name='profit-loss'),
+    path('projects-profit', views.projects_profit, name='projects-profit'),
 ]
