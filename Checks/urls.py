a9d1c5f9337e677bd2fcb8854190b2784d81b1c5
Elad Reuveni <elad@nummularii.com>
2019-07-27 23:29:40 +0300

    fixed urls

--- Checks/urls.py
+++ Checks/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('checks/', views.check_list),
-    path('checks/add', views.check_add),
+    path('checks/', views.check_list, name='checks-list'),
+    path('checks/add', views.check_add, name='checks-add'),
     path('checks/<int:id>', views.check_edit),
     path('checks/<int:pk>/del', views.PaymentCheckDelete.as_view()),
          
-    path('employeechecks/', views.employeecheck_list),
-    path('employeechecks/add', views.employeecheck_add),
+    path('employeechecks/', views.employeecheck_list, name='employee-checks-list'),
+    path('employeechecks/add', views.employeecheck_add, name='employee-checks-add'),
     path('employeechecks/<int:id>', views.employeecheck_edit),
     path('employeechecks/<int:id>/del', views.EmployeeCheckDelete.as_view()),
 ]
