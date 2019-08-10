71f18918de6079c0e45dca98d7a813236affd8df
Elad Reuveni <elad@nummularii.com>
2019-08-10 15:58:59 +0300

    set DEBUG=True

--- NiceHouse/settings.py
+++ NiceHouse/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b1&^9&3e48hs#1p+mtypy^8-ho)r%-3^inrp0d9haz0$-xml0o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
