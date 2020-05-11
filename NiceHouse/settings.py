80c8d02b7a46ac395aeb0ff5f44a0f60c86669ef
Elad Reuveni <elad@nummularii.com>
2020-05-11 22:45:48 +0300

    added email settings

--- NiceHouse/settings.py
+++ NiceHouse/settings.py
@@ -127,6 +127,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'nevehair@gmail.com'
+EMAIL_HOST_PASSWORD = '8Z%!z\rV~?!bfG})'
+
 SESSION_COOKIE_AGE = 8 * 60 * 60 # 8 Hours
 
 # Internationalization
