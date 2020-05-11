c0278d35cbfbb0f9b5812df5d1ef41b5b9aa4f5e
Elad Reuveni <elad@nummularii.com>
2020-05-11 23:02:26 +0300

    changed TLS to SSL

--- NiceHouse/settings.py
+++ NiceHouse/settings.py
@@ -129,8 +129,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'nevehair@gmail.com'
 EMAIL_HOST_PASSWORD = '8Z%!zV~?!bfG})'
 
